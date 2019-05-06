d87342d6a1dae534e65a3830e1d0e445d5b0f2e4
Peter <peterfrankfoerster@gmail.com>
2019-05-06 13:02:37 +0200

    peter

--- photocathode/code/convergence_study.m
+++ photocathode/code/convergence_study.m
@@ -1,5 +1,6 @@
 pkg load geopdes;
-degree = 2;
+degree_ref = 5;
+degree = 4;
 N_it = 4;
 geometry_file = 'photocathode_insulator';
 
@@ -9,9 +10,10 @@ voltage = -60e3;
 [problem_data, method_data] = init_potential (geometry_file, voltage);
 
 % reference solution
-method_data.degree = [5 5];
+method_data.degree = [degree_ref degree_ref];
+method_data.regularity = [degree_ref-1 degree_ref-1];
 method_data.nsub = [2^N_it 2^N_it];
-method_data.nquad = [6 6];
+method_data.nquad = [degree_ref+1 degree_ref+1];
 tic;
 [geometry, msh_ref, space_ref, u] = mp_solve_laplace_mod (problem_data, method_data);
 [h1_ref, l2_ref] = mp_sp_h1 (space_ref, msh_ref, u, space_ref, geometry);
@@ -19,7 +21,9 @@ fprintf('time elapsed for reference solution:%d\n', toc);
 
 % convergence study
 method_data.degree = [degree degree];
-method_data.nquad = [degree+1 degree+1];
+method_data.regularity = [degree-1 degree-1];
+% evaluate with the same number as the reference?
+method_data.nquad = [degree_ref+1 degree_ref+1];
 for iit=0:N_it
   fprintf('\niteration with nsub = %d\n', 2^iit);
   method_data.nsub = [2^iit 2^iit];
@@ -31,7 +35,7 @@ for iit=0:N_it
   fprintf('time elapsed for norm computation:%d\n', toc);
 end
 
-save([geometry_file '_ref=5_degree=' num2str(degree) '_N_it=' num2str(N_it) '.mat'], 'N_it', 'h1_conv', 'h1_ref', 'l2_conv', 'l2_ref');
+save([geometry_file '_ref=' num2str(degree_ref) '_degree=' num2str(degree) '_N_it=' num2str(N_it) '.mat'], 'N_it', 'h1_conv', 'h1_ref', 'l2_conv', 'l2_ref');
 
 % signal that the program is finished
 x = linspace(1, 20, 8000);
