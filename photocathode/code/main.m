6bd39a042760da3489c6e7c7b32682c98ea720bf
peter <peterfrankfoerster@gmail.com>
2019-05-07 19:30:19 +0200

    peter

--- photocathode/code/main.m
+++ photocathode/code/main.m
@@ -3,8 +3,8 @@ pkg load geopdes;
 
 %% plot the geometry
 %% plain, kink, insulator
-geometry_file = 'photocathode_insulator';
-[geometry, boundaries, interfaces, ~, boundary_interfaces] = mp_geo_load ([geometry_file '.txt']);
+% geometry_file = 'photocathode_insulator';
+% [geometry, boundaries, interfaces, ~, boundary_interfaces] = mp_geo_load ([geometry_file '.txt']);
 % nsub = 5;
 % width = 4;
 % options.numbers = 1;
@@ -13,24 +13,25 @@ geometry_file = 'photocathode_insulator';
 % plot_geometry (geometry, nsub, width, options, boundaries);
 
 %% solve for the potential
-voltage = -60e3;
-[problem_data, method_data] = init_potential (geometry, boundaries, interfaces, boundary_interfaces, voltage);
-[geometry, msh, space, u] = mp_solve_laplace_mod (problem_data, method_data);
+% voltage = -60e3;
+% [problem_data, method_data] = init_potential (geometry, boundaries, interfaces, boundary_interfaces, voltage);
+% [geometry, msh, space, u] = mp_solve_laplace_mod (problem_data, method_data);
 
 %% plot the potential and the absolute value of the gradient
-figure;
+% figure;
 % nsub_x = method_data.nsub(1);
 % nsub_y = method_data.nsub(2);
-nsub_x = nsub_y = 32;
-plot_potential (nsub_x, nsub_y, u, space, geometry);
-figure;
-plot_gradient (nsub_x, nsub_y, u, space, geometry);
+% nsub_x = nsub_y = 32;
+% plot_potential (nsub_x, nsub_y, u, space, geometry);
+% figure;
+% plot_gradient (nsub_x, nsub_y, u, space, geometry);
 
 %% convergence study (with absolute error)
-% filename = 'photocathode_plain_degree=2_nsub=var';
-% figure;
-% plot_convergence_study (filename);
-
+degree = 2;
+filename = 'photocathode_insulator_ref=5_degree=2_N_it=4';
+figure;
+plot_convergence_study (filename, degree);
+return
 % signal that the program is finished
 x = linspace(1, 20, 8000);
 Y = sin(2*pi*440*x);
--- photocathode/code/plot/plot_convergence_study.m
+++ photocathode/code/plot/plot_convergence_study.m
@@ -1,17 +1,18 @@
-function [] = plot_convergence_study (filename)
-  % N_it, errh1, errl2
+function [] = plot_convergence_study (filename, degree)
+  % N_it, h1_ref, l2_ref, h1_conv, l2_conv
   load(filename);
 
   h = 1./2.^(0:N_it);
-  h_exp3 = h.^3;
-  h_exp2 = h.^2;
+  h_expl2 = h.^(degree+1);
+  h_exph1 = h.^degree;
 
-  plot(log(h), log(errl2/60e3), log(h), log(h_exp3)+log(errl2(1)/60e3));
+  errl2 = (l2_conv - l2_ref)/l2_ref;
+  errh1 = (h1_conv - h1_ref)/h1_ref;
+
+  plot(h, errl2, h, h_expl2*errl2(1));
   title('error in L_2 norm');
-  legend('convergence', 'expected');
 
   figure;
-  plot(log(h), log(errh1/2e6), log(h), log(h_exp2)+log(errh1(1)/2e6));
+  loglog(h, errh1, h, h_exph1*errh1(1));
   title('error in H_1 norm');
-  legend('convergence', 'expected');
 end
