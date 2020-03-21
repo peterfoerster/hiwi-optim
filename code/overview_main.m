eff484c887bc48d866a3a000c76c8adfd6d140ae
peter <peterfrankfoerster@gmail.com>
2020-03-21 22:51:03 +0100

    peter

--- code/overview_main.m
+++ code/overview_main.m
@@ -1,7 +1,7 @@
 pkg load geopdes;
 
 % geometry_file = 'photocathode_200kV_v5';
-geometry_file = 'photocathode_200kV_optim_order=3';
+geometry_file = 'photocathode_200kV_optim_order=3_run18';
 
 [geometry, boundaries] = mp_geo_load ([geometry_file '.txt']);
 
@@ -14,8 +14,8 @@ geometry_file = 'photocathode_200kV_optim_order=3';
 % write_geometry (geometry);
 
 % plot geometry
-plot_geometry (geometry, boundaries);
-return
+% plot_geometry (geometry, boundaries);
+
 % solve electrostatic problem
 [problem_data, method_data] = setup_problem (geometry_file);
 tic;
@@ -23,9 +23,9 @@ tic;
 fprintf('\ntime elapsed for solution: %d min\n', toc/60);
 
 % plot absolute value of electric field and write .dat files
-% vtk_pts = {linspace(0, 1, 8), linspace(0, 1, 8)};
-% plot_es_mp (phi, space, geometry, vtk_pts, ['E_degree=' num2str(method_data.degree(1)) '_nsub=' num2str(method_data.nsub(1))]);
-% view(2);
+vtk_pts = {linspace(0, 1, 8), linspace(0, 1, 8)};
+plot_es_mp (phi, space, geometry, vtk_pts, ['E_degree=' num2str(method_data.degree(1)) '_nsub=' num2str(method_data.nsub(1))]);
+view(2);
 return
 % write .vtk files
 sp_to_vtk (phi, space, geometry, method_data.nsub, ['E_degree=' num2str(method_data.degree(1)) '_nsub=' num2str(method_data.nsub(1))], 'E', 'gradient');
--- code/setup/setup_problem.m
+++ code/setup/setup_problem.m
@@ -9,11 +9,11 @@ function [problem_data, method_data] = setup_problem (geometry_file)
    v_el = -200e3;
    v_ar = 0;
    problem_data.f = @(x,y) zeros(size(x));
-   problem_data.g = @(x,y,ib) nm_bc (ib, x, y);
    problem_data.h = @(x,y,ib) drl_bc (ib, x, y, v_el, v_ar);
+   problem_data.g = @(x,y,ib) nm_bc (ib, x, y);
 
    method_data.degree     = [3 3];
    method_data.regularity = method_data.degree - 1;
-   method_data.nsub       = [16 16];
+   method_data.nsub       = [2^4 2^4];
    method_data.nquad      = method_data.degree + 1;
 end
