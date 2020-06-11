edbdfa26c0b42213531fa45fdb83625424e174bd
Peter <peterfrankfoerster@gmail.com>
2020-06-11 11:31:03 +0200

    fixed bug in cutting of the NURBS

--- code/overview_main.m
+++ code/overview_main.m
@@ -1,8 +1,8 @@
 % Plots the geometry and computes the field solution.
 % geometry_file = 'geometry_v6';
 % geometry_file = 'electrode_v6';
-geometry_file = 'v6_opt_order=5_run3';
-order = 5;
+geometry_file = 'v6_opt_order=4';
+order = 4;
 
 [geometry, boundaries] = mp_geo_load ([geometry_file '.txt']);
 
